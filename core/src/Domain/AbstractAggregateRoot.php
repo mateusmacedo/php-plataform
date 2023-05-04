8a5f1818a24415812ac684484f65df1176ed44b5
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-05-04 10:18:44 -0300

    feat(core): :sparkles: aggregate root events methods
    implements methods to record, release and clear events.

--- core/src/Domain/AbstractAggregateRoot.php
+++ core/src/Domain/AbstractAggregateRoot.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Core\Domain;
 
+use Generator;
+use SplObjectStorage;
+
 /**
  * Class AbstractAggregateRoot represents an abstract aggregate root is base class for all aggregate roots.
  *
@@ -13,4 +16,53 @@ namespace Core\Domain;
  */
 abstract class AbstractAggregateRoot extends AbstractEntity
 {
+    protected SplObjectStorage $events;
+
+    /**
+     * AbstractAggregateRoot constructor is responsible for construct a new abstract aggregate root.
+     *
+     * @param string|null $id
+     * @param string|null $createdAt
+     * @param string|null $updatedAt
+     * @param string|null $deletedAt
+     */
+    public function __construct(
+        ?string $id = null,
+        ?string $createdAt = null,
+        ?string $updatedAt = null,
+        ?string $deletedAt = null
+    ) {
+        parent::__construct($id, $createdAt, $updatedAt, $deletedAt);
+        $this->clearEvents();
+    }
+
+    /**
+     * Method to add a event to the aggregate root
+     *
+     * @param AbstractDomainEvent $event
+     */
+    protected function record(AbstractDomainEvent $event): void
+    {
+        $this->events->attach($event);
+    }
+
+    /**
+     * Method to get all events of the aggregate root
+     *
+     * @return Generator
+     */
+    public function releaseEvents(): Generator
+    {
+        foreach ($this->events as $event) {
+            yield $event;
+        }
+    }
+
+    /**
+     * Method to clear all events of the aggregate root
+     */
+    public function clearEvents(): void
+    {
+        $this->events = new SplObjectStorage();
+    }
 }
