46e4f921aec56397944669fea295b422df31d222
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-05-04 02:50:11 -0300

    feat(core): :sparkles: domain exception
    implement a base domain exception class

---
+++ core/src/Domain/DomainException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Domain;
+
+use DomainException as BaseDomainException;
+
+/**
+ * Class DomainException a generic domain exception.
+ *
+ * @package Core\Domain
+ * @author Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
+ * @version 0.0.1
+ */
+class DomainException extends BaseDomainException
+{
+}
