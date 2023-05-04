9f4ce4697d2def6115f7e626ae99fc04ed944f18
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-05-04 02:49:35 -0300

    feat(core): :sparkles: factory method contract
    define a interface to factory method pattern

---
+++ core/src/Domain/FactoryMethodInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Domain;
+
+/**
+ * Interface FactoryInterface
+ *
+ * @package Core\Domain
+ * @author Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
+ * @version 0.0.1
+ */
+interface FactoryMethodInterface
+{
+    /**
+     * Method to create a new instance of the class
+     *
+     * @param mixed $data
+     * @return mixed
+     */
+    public function create($data);
+}
