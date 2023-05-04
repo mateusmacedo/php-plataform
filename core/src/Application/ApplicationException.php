d73170912e5d39450d27ff04ed8d4ae8cc3055e7
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-05-04 10:17:37 -0300

    refactor(core): :label: specialized exception
    extend application error with domain error to piramid dependency

--- core/src/Application/ApplicationException.php
+++ core/src/Application/ApplicationException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Core\Application;
 
-use Exception;
+use Core\Domain\DomainException;
 
 /**
  * Class ApplicationException is the base exception for the application layer
@@ -13,6 +13,6 @@ use Exception;
  * @author Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
  * @version 0.0.1
  */
-class ApplicationException extends Exception
+class ApplicationException extends DomainException
 {
 }
